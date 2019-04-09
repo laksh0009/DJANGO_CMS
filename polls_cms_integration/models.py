5e138dd29aa3def13ae5611f6fc356ee1305f0ef
laksh <lakshhemke1@gmail.com>
2019-04-09 15:38:44 +0530

    New 2nd commit

--- polls_cms_integration/models.py
+++ polls_cms_integration/models.py
@@ -3,6 +3,9 @@ from cms.models import CMSPlugin
 from polls.models import Poll
 "THE REAL ONE"
 "THIS IS FOR TESTING"
+"THIS IS 2ND ONE"
+"THIS IS 2ND ONE"
+"THIS IS 2ND ONE"
 class PollPluginModel(CMSPlugin):
     poll=models.ForeignKey(Poll)
 
