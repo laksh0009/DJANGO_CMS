388e4e19663a459099b8dfe166a87f21e7b74f8c
laksh <lakshhemke1@gmail.com>
2019-04-09 15:37:02 +0530

    hiiiii

--- polls_cms_integration/models.py
+++ polls_cms_integration/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from cms.models import CMSPlugin
 from polls.models import Poll
-
+"THE REAL ONE"
 "THIS IS FOR TESTING"
 class PollPluginModel(CMSPlugin):
     poll=models.ForeignKey(Poll)
