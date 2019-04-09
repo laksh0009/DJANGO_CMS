851af56d75af4ff65f25d057e5aedccb1343301a
laksh0009 <48160218+laksh0009@users.noreply.github.com>
2019-04-09 16:13:28 +0530

    Update models.py

--- polls_cms_integration/models.py
+++ polls_cms_integration/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from cms.models import CMSPlugin
 from polls.models import Poll
 
+"This is my new thing"
 class PollPluginModel(CMSPlugin):
     poll=models.ForeignKey(Poll)
 
